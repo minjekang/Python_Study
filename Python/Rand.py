6f2e59f7644713a25d001342d8eac6cd42d9c0d6
minjekang <kangminje777@gmail.com>
2021-09-11 19:05:17 +0900

    Random

---
+++ Python/Rand.py
@@ -0,0 +1,36 @@
+from random import *
+
+# print(random()) 
+# print(random() * 10) # 0.0 ~ 10.0
+# print(int (random() * 10)) # 0 ~ 10
+# print(int (random() * 10))
+# print(int (random() * 10)) 
+
+# print(int (random() * 10 + 1)) # 1 ~ 10
+# print(int (random() * 10 + 1)) # 1 ~ 10
+# print(int (random() * 10 + 1)) # 1 ~ 10
+# print(int (random() * 10 + 1)) # 1 ~ 10
+# print(int (random() * 10 + 1)) # 1 ~ 10
+
+
+# print (int(random() * 45) + 1)
+# print (int(random() * 45) + 1)
+# print (int(random() * 45) + 1)
+# print (int(random() * 45) + 1)
+# print (int(random() * 45) + 1)
+# print (int(random() * 45) + 1)
+
+# print(randrange(1,46)) #1 ~ 45
+# print(randrange(1,46)) #1 ~ 45
+# print(randrange(1,46)) #1 ~ 45
+# print(randrange(1,46)) #1 ~ 45
+
+# print(randint(1,45)) # 1~45
+# print(randint(1,45)) # 1~45
+# print(randint(1,45)) # 1~45
+# print(randint(1,45)) # 1~45
+
+
+# Quiz
+i = randint(4,28)
+print("오프라인 스터디 모임은 매월" + str(i) +"일 입니다.")
