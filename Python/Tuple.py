7e935805557496aeb74f937cf223b77c018c8a0e
minjekang <kangminje777@gmail.com>
2021-09-13 16:18:56 +0900

    튜플

---
+++ Python/Tuple.py
@@ -0,0 +1,15 @@
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+
+#menu.add("생선까스")    튜플은 add기능 없음
+
+#name = "강민제"
+#age = 17
+#hobby = "코딩"
+#print(name,age,hobby)
+
+
+(name, age, hobby) = ("강민제", 17, "코딩")
+print(name,age,hobby)
