0038ff9542ccaf3087baef670b7e55a662c755b7
minjekang <kangminje777@gmail.com>
2021-09-15 20:52:49 +0900

    Conditional

---
+++ Python/Conditional.py
@@ -0,0 +1,48 @@
+############ if ###################
+
+# weather = input("오늘 날씨는 어때요?")
+# if weather == "비" or "눈":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물 필요 없어요")
+
+
+# temp = int(input("기온이 어때요?"))
+# if 30 <= temp:
+#     print("너무 더움")
+# elif 10 <= temp and temp < 30:
+#     print("괜찮은 날씨")
+# elif 0 <= temp and temp < 10:
+#     print("약간 추움")
+# else:
+#     print("너무 추움")
+
+############ if ###################
+
+
+############ for ###################
+
+# print("대기번호: 1")
+# print("대기번호: 2")
+# print("대기번호: 3")
+# print("대기번호: 4")
+
+# for waiting_n in range(1, 6):
+#     print("대기번호: {0}".format(waiting_n))
+
+# starbucks = ["아이언맨", "토르", "그루트"]
+# for customer in starbucks:
+#     print("{0}님, 커피가 준비 되었습니다".format(customer))
+
+
+############ for ###################
+
+
+############ while ###################
+
+
+
+
+############ while ###################
