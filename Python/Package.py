266884ecb9c931751b6e1d0f3b0879ee8cb8bc3c
minjekang <kangminje777@gmail.com>
2021-09-21 01:34:36 +0900

    Package

---
+++ Python/Package.py
@@ -0,0 +1,18 @@
+# import travel.Thailand
+# # import travel.ThailandPackage   X
+# trip_to = travel.Thailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel.Thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# from travel import Vietnam
+# trip_to = Vietnam.VietnamPackage()
+# trip_to.detail()
+
+# from random import*
+from travel import*
+#trip_to = Vietnam.VietnamPackage()
+trip_to = Thailand.ThailandPackage()
+trip_to.detail()
