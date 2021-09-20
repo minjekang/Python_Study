e298eec69088e3de16c09fc52c4ba5bba5ef76da
minjekang <kangminje777@gmail.com>
2021-09-20 23:43:45 +0900

    Starcraft

--- Python/Class.py
+++ Python/Class.py
@@ -119,20 +119,9 @@ class FlyableAttackUnit(AttackUnit, Flyable):
 # 건물
 class BuildingUnit(Unit):
     def __init__(self, name, hp, location):
-        pass
-
-
-# 서프라이 디폿 : 건물, 1개 = 8유닛.
-supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
-
-def game_start():
-    print("[알림] 새로운 게임을 시작합니다.")
-
-def game_over():
-    pass
-
-game_start()
-game_over()
+        #Unit.__init__(self, name, hp, 0)
+        super().__init__(name, hp, 0)
+        self.location = location
 
 
 # # 발키리 : 공중 공격 유닛, 한 번에 14발 미사일 발사
